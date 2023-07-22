24543b95b990e4be1c21155ff82280e81c266c6f
Angela Christabel <angela.christabel@gmail.com>
2023-07-23 00:38:31 +0700

    latest version without beacon detection

---
+++ MRTJMobile/Application/PartialSheetManager.swift
@@ -0,0 +1,8 @@
+//
+//  PartialSheetManager.swift
+//  MRTJMobile
+//
+//  Created by Angela Christabel on 22/07/23.
+//
+
+import Foundation
---
+++ MRTJMobile/Features/Schedule/Data/Datasources/TrainData.swift
@@ -0,0 +1,8 @@
+//
+//  TrainData.swift
+//  MRTJMobile
+//
+//  Created by Angela Christabel on 22/07/23.
+//
+
+import Foundation
