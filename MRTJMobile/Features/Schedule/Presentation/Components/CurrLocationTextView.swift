ea64a36fb113d2e01e560bdac8e14619db5e6cc0
Angela Christabel <angela.christabel@gmail.com>
2023-07-24 12:34:46 +0700

    picker working

--- MRTJMobile/Features/Schedule/Presentation/Components/CurrLocationTextView.swift
+++ MRTJMobile/Features/Schedule/Presentation/Components/CurrLocationTextView.swift
@@ -8,19 +8,36 @@
 import SwiftUI
 
 struct CurrLocationTextView: View {
-    var station: String
+    @State var stationName: String
+    @Binding var station: Station?
     
     var body: some View {
         HStack {
             Image(systemName: "location.fill")
-            Text("You are now at **\(station)**")
+            Text("You are now at")
                 .font(.subheadline)
+            Picker("Choose a station", selection: $stationName) {
+                ForEach(stationNames, id: \.self) {
+                    Text($0)
+                }
+            }
+            .onChange(of: stationName) { newValue in
+                station = stations[newValue]
+            }
         }
     }
 }
 
 struct CurrLocationTextView_Previews: PreviewProvider {
     static var previews: some View {
-        CurrLocationTextView(station: "Blok M BCA")
+        CurrLocationTextView(stationName: "Lebak Bulus Grab", station: .constant(Station(name: "Lebak Bulus Grab", firstLine: MRTLine(destination: "Bundaran HI", direction: .right, trains: [
+            TrainTime(train: trains[0], arrival: Date(timeIntervalSinceNow: 300)),
+            TrainTime(train: trains[1], arrival: Date(timeIntervalSinceNow: 600)),
+            TrainTime(train: trains[2], arrival: Date(timeIntervalSinceNow: 900)),
+        ]), secondLine: MRTLine(destination: "Bundaran HI", direction: .right, trains: [
+            TrainTime(train: trains[3], arrival: Date(timeIntervalSinceNow: 300)),
+            TrainTime(train: trains[4], arrival: Date(timeIntervalSinceNow: 700)),
+            TrainTime(train: trains[5], arrival: Date(timeIntervalSinceNow: 800)),
+        ]), latitude: -6.289361581369295, longitude: 106.77411877729375)))
     }
 }
--- MRTJMobile/Features/Schedule/Presentation/Pages/ScheduleView.swift
+++ MRTJMobile/Features/Schedule/Presentation/Pages/ScheduleView.swift
@@ -10,33 +10,39 @@ import SwiftUI
 struct ScheduleView: View {
     @EnvironmentObject var locationManager: LocationManager
     @ObservedObject var scheduleVM = ScheduleViewModel()
+    @State var station: Station? = stations["Lebak Bulus Grab"]
     
     @State private var available = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: Cons.spacing) {
-            HStack {
-                CurrLocationTextView(station: locationManager.lastStation?.name ?? "unknown")
-                Spacer()
+        ScrollView {
+            VStack(alignment: .leading, spacing: Cons.spacing) {
                 HStack {
-                    Image(systemName: "circle.fill")
-                        .foregroundColor(.red)
-                    Text("**LIVE**")
-                        .font(.subheadline)
+                    CurrLocationTextView(stationName: station?.name ?? "unknown", station: $station)
+                    Spacer()
+                    HStack {
+                        Image(systemName: "circle.fill")
+                            .foregroundColor(.red)
+                        Text("**LIVE**")
+                            .font(.subheadline)
+                    }
                 }
-            }
-            
-            if let station = locationManager.lastStation {
-                ScheduleCardView(line: station.firstLine, num: 1)
+                
+                if let st = self.station {
+                    ScheduleCardView(line: st.firstLine, num: 1)
 
-                if let secondLine = station.secondLine {
-                    ScheduleCardView(line: secondLine, num: 2)
+                    if let secondLine = st.secondLine {
+                        ScheduleCardView(line: secondLine, num: 2)
+                    }
                 }
+                
+                Spacer()
             }
-            
-            Spacer()
+            .padding(Cons.padding)
         }
-        .padding(Cons.padding)
+        .onAppear {
+            station = locationManager.lastStation
+        }
     }
 }
 
